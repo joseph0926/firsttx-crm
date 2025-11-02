24bbbd75d8abebe538d408cb5e1beeec5ef093b2
joseph0926 <joseph0926@naver.com>
2025-11-02 10:56:49 +0900

    feat(client): implement GraphQL client with urql and codegen - auth, user

--- apps/client/src/gql/gql.ts
+++ apps/client/src/gql/gql.ts
@@ -14,14 +14,20 @@ import type { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-
  * Learn more about it here: https://the-guild.dev/graphql/codegen/plugins/presets/preset-client#reducing-bundle-size
  */
 type Documents = {
+    "mutation RequestMagicLink($input: RequestMagicLinkInput!) {\n  requestMagicLink(input: $input) {\n    success\n    message\n    expiresInMinutes\n  }\n}\n\nmutation VerifyMagicLink($input: VerifyMagicLinkInput!) {\n  verifyMagicLink(input: $input) {\n    accessToken\n    user {\n      ...UserDetail\n    }\n  }\n}": typeof types.RequestMagicLinkDocument,
+    "query Me {\n  me {\n    ...UserDetail\n  }\n}": typeof types.MeDocument,
     "fragment ContactCore on Contact {\n  id\n  name\n}\n\nfragment ContactContactInfo on Contact {\n  email\n  phone\n}\n\nfragment ContactBusinessInfo on Contact {\n  company\n  position\n}\n\nfragment ContactStatus on Contact {\n  status\n  priority\n}\n\nfragment ContactMetadata on Contact {\n  tags\n  notes\n}\n\nfragment ContactTimestamps on Contact {\n  createdAt\n  updatedAt\n  lastContactedAt\n}\n\nfragment ContactListItem on Contact {\n  ...ContactCore\n  ...ContactContactInfo\n  ...ContactStatus\n  lastContactedAt\n}\n\nfragment ContactCard on Contact {\n  ...ContactListItem\n  ...ContactBusinessInfo\n  tags\n}\n\nfragment ContactDetail on Contact {\n  ...ContactCore\n  ...ContactContactInfo\n  ...ContactBusinessInfo\n  ...ContactStatus\n  ...ContactMetadata\n  ...ContactTimestamps\n}": typeof types.ContactCoreFragmentDoc,
     "mutation CreateContact($input: CreateContactInput!) {\n  createContact(input: $input) {\n    ...ContactDetail\n  }\n}\n\nmutation UpdateContact($id: String!, $input: UpdateContactInput!) {\n  updateContact(id: $id, input: $input) {\n    ...ContactDetail\n  }\n}\n\nmutation RemoveContact($id: String!) {\n  removeContact(id: $id) {\n    ...ContactCore\n  }\n}": typeof types.CreateContactDocument,
     "query GetContacts {\n  contacts {\n    ...ContactListItem\n  }\n}\n\nquery GetContact($id: String!) {\n  contact(id: $id) {\n    ...ContactDetail\n  }\n}": typeof types.GetContactsDocument,
+    "fragment UserCore on User {\n  id\n  email\n  name\n}\n\nfragment UserTimestamps on User {\n  createdAt\n  updatedAt\n}\n\nfragment UserDetail on User {\n  ...UserCore\n  ...UserTimestamps\n}": typeof types.UserCoreFragmentDoc,
 };
 const documents: Documents = {
+    "mutation RequestMagicLink($input: RequestMagicLinkInput!) {\n  requestMagicLink(input: $input) {\n    success\n    message\n    expiresInMinutes\n  }\n}\n\nmutation VerifyMagicLink($input: VerifyMagicLinkInput!) {\n  verifyMagicLink(input: $input) {\n    accessToken\n    user {\n      ...UserDetail\n    }\n  }\n}": types.RequestMagicLinkDocument,
+    "query Me {\n  me {\n    ...UserDetail\n  }\n}": types.MeDocument,
     "fragment ContactCore on Contact {\n  id\n  name\n}\n\nfragment ContactContactInfo on Contact {\n  email\n  phone\n}\n\nfragment ContactBusinessInfo on Contact {\n  company\n  position\n}\n\nfragment ContactStatus on Contact {\n  status\n  priority\n}\n\nfragment ContactMetadata on Contact {\n  tags\n  notes\n}\n\nfragment ContactTimestamps on Contact {\n  createdAt\n  updatedAt\n  lastContactedAt\n}\n\nfragment ContactListItem on Contact {\n  ...ContactCore\n  ...ContactContactInfo\n  ...ContactStatus\n  lastContactedAt\n}\n\nfragment ContactCard on Contact {\n  ...ContactListItem\n  ...ContactBusinessInfo\n  tags\n}\n\nfragment ContactDetail on Contact {\n  ...ContactCore\n  ...ContactContactInfo\n  ...ContactBusinessInfo\n  ...ContactStatus\n  ...ContactMetadata\n  ...ContactTimestamps\n}": types.ContactCoreFragmentDoc,
     "mutation CreateContact($input: CreateContactInput!) {\n  createContact(input: $input) {\n    ...ContactDetail\n  }\n}\n\nmutation UpdateContact($id: String!, $input: UpdateContactInput!) {\n  updateContact(id: $id, input: $input) {\n    ...ContactDetail\n  }\n}\n\nmutation RemoveContact($id: String!) {\n  removeContact(id: $id) {\n    ...ContactCore\n  }\n}": types.CreateContactDocument,
     "query GetContacts {\n  contacts {\n    ...ContactListItem\n  }\n}\n\nquery GetContact($id: String!) {\n  contact(id: $id) {\n    ...ContactDetail\n  }\n}": types.GetContactsDocument,
+    "fragment UserCore on User {\n  id\n  email\n  name\n}\n\nfragment UserTimestamps on User {\n  createdAt\n  updatedAt\n}\n\nfragment UserDetail on User {\n  ...UserCore\n  ...UserTimestamps\n}": types.UserCoreFragmentDoc,
 };
 
 /**
@@ -41,6 +47,14 @@ export function graphql(source: string): unknown;
 /**
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
+export function graphql(source: "mutation RequestMagicLink($input: RequestMagicLinkInput!) {\n  requestMagicLink(input: $input) {\n    success\n    message\n    expiresInMinutes\n  }\n}\n\nmutation VerifyMagicLink($input: VerifyMagicLinkInput!) {\n  verifyMagicLink(input: $input) {\n    accessToken\n    user {\n      ...UserDetail\n    }\n  }\n}"): (typeof documents)["mutation RequestMagicLink($input: RequestMagicLinkInput!) {\n  requestMagicLink(input: $input) {\n    success\n    message\n    expiresInMinutes\n  }\n}\n\nmutation VerifyMagicLink($input: VerifyMagicLinkInput!) {\n  verifyMagicLink(input: $input) {\n    accessToken\n    user {\n      ...UserDetail\n    }\n  }\n}"];
+/**
+ * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
+ */
+export function graphql(source: "query Me {\n  me {\n    ...UserDetail\n  }\n}"): (typeof documents)["query Me {\n  me {\n    ...UserDetail\n  }\n}"];
+/**
+ * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
+ */
 export function graphql(source: "fragment ContactCore on Contact {\n  id\n  name\n}\n\nfragment ContactContactInfo on Contact {\n  email\n  phone\n}\n\nfragment ContactBusinessInfo on Contact {\n  company\n  position\n}\n\nfragment ContactStatus on Contact {\n  status\n  priority\n}\n\nfragment ContactMetadata on Contact {\n  tags\n  notes\n}\n\nfragment ContactTimestamps on Contact {\n  createdAt\n  updatedAt\n  lastContactedAt\n}\n\nfragment ContactListItem on Contact {\n  ...ContactCore\n  ...ContactContactInfo\n  ...ContactStatus\n  lastContactedAt\n}\n\nfragment ContactCard on Contact {\n  ...ContactListItem\n  ...ContactBusinessInfo\n  tags\n}\n\nfragment ContactDetail on Contact {\n  ...ContactCore\n  ...ContactContactInfo\n  ...ContactBusinessInfo\n  ...ContactStatus\n  ...ContactMetadata\n  ...ContactTimestamps\n}"): (typeof documents)["fragment ContactCore on Contact {\n  id\n  name\n}\n\nfragment ContactContactInfo on Contact {\n  email\n  phone\n}\n\nfragment ContactBusinessInfo on Contact {\n  company\n  position\n}\n\nfragment ContactStatus on Contact {\n  status\n  priority\n}\n\nfragment ContactMetadata on Contact {\n  tags\n  notes\n}\n\nfragment ContactTimestamps on Contact {\n  createdAt\n  updatedAt\n  lastContactedAt\n}\n\nfragment ContactListItem on Contact {\n  ...ContactCore\n  ...ContactContactInfo\n  ...ContactStatus\n  lastContactedAt\n}\n\nfragment ContactCard on Contact {\n  ...ContactListItem\n  ...ContactBusinessInfo\n  tags\n}\n\nfragment ContactDetail on Contact {\n  ...ContactCore\n  ...ContactContactInfo\n  ...ContactBusinessInfo\n  ...ContactStatus\n  ...ContactMetadata\n  ...ContactTimestamps\n}"];
 /**
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
@@ -50,6 +64,10 @@ export function graphql(source: "mutation CreateContact($input: CreateContactInp
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
 export function graphql(source: "query GetContacts {\n  contacts {\n    ...ContactListItem\n  }\n}\n\nquery GetContact($id: String!) {\n  contact(id: $id) {\n    ...ContactDetail\n  }\n}"): (typeof documents)["query GetContacts {\n  contacts {\n    ...ContactListItem\n  }\n}\n\nquery GetContact($id: String!) {\n  contact(id: $id) {\n    ...ContactDetail\n  }\n}"];
+/**
+ * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
+ */
+export function graphql(source: "fragment UserCore on User {\n  id\n  email\n  name\n}\n\nfragment UserTimestamps on User {\n  createdAt\n  updatedAt\n}\n\nfragment UserDetail on User {\n  ...UserCore\n  ...UserTimestamps\n}"): (typeof documents)["fragment UserCore on User {\n  id\n  email\n  name\n}\n\nfragment UserTimestamps on User {\n  createdAt\n  updatedAt\n}\n\nfragment UserDetail on User {\n  ...UserCore\n  ...UserTimestamps\n}"];
 
 export function graphql(source: string) {
   return (documents as any)[source] ?? {};
